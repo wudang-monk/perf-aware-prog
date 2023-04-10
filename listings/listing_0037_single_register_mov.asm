6f76ccfd9a8859bda2e747747bbd560b528b4f00
Pedro Munoz <wudang.monk@gmail.com>
2023-04-09 21:06:54 -0700

    add all listings from repo, remove local modified copies

--- listings/listing_0037_single_register_mov.asm
+++ listings/listing_0037_single_register_mov.asm
@@ -11,26 +11,9 @@
 ; ========================================================================
 
 ; ========================================================================
-; LISTING 44
+; LISTING 37
 ; ========================================================================
 
 bits 16
 
-mov ax, 1
-mov bx, 2
-mov cx, 3
-mov dx, 4
-
-mov sp, ax
-mov bp, bx
-mov si, cx
-mov di, dx
-
-mov dx, sp
-mov cx, bp
-mov bx, si
-mov ax, di
-
-add ax, bx
-add bx, cx
-add cx, dx
+mov cx, bx
--- listings/listing_0040_challenge_movs.asm
+++ listings/listing_0040_challenge_movs.asm
@@ -11,14 +11,28 @@
 ; ========================================================================
 
 ; ========================================================================
-; LISTING 49
+; LISTING 40
 ; ========================================================================
 
 bits 16
 
-mov cx, 3
-mov bx, 1000
-loop_start:
-add bx, 10
-sub cx, 1
-jnz loop_start
+; Signed displacements
+mov ax, [bx + di - 37]
+mov [si - 300], cx
+mov dx, [bx - 32]
+
+; Explicit sizes
+mov [bp + di], byte 7
+mov [di + 901], word 347
+
+; Direct address
+mov bp, [5]
+mov bx, [3458]
+
+; Memory-to-accumulator test
+mov ax, [2555]
+mov ax, [16]
+
+; Accumulator-to-memory test
+mov [2554], ax
+mov [15], ax
